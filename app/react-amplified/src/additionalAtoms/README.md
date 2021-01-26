9daa70ffee394104a1faba85398be92f9361c4a9
ryuji <forest.book.programmer@gmail.com>
2021-01-26 13:59:59 +0900

    ディレクトリ構成変更中

---
+++ app/react-amplified/src/additionalAtoms/README.md
@@ -0,0 +1,2 @@
+このディレクトリは、polaris に定義されていない atomic component を定義する。
+または、polaris をカスタマイズした component を定義する。
--- app/react-amplified/src/index.js
+++ app/react-amplified/src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import Amplify from "aws-amplify";
-import awsExports from "./aws-exports";
+import awsExports from "./Cognito/aws-exports";
 Amplify.configure(awsExports);
 
 ReactDOM.render(
